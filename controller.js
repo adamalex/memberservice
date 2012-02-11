9d2236c1425be71d04bc7eeeefb82b27ad10445d
Adam Alexander <adamalex@gmail.com>
2012-02-11 13:17:52 -0500

    Completed CRUD passthrough

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@ var thrift = require('thrift');
 var _ = require('underscore');
 
 var MemberStore = require('../memberstore/gen-nodejs/MemberStore');
+var Member = require('../memberstore/gen-nodejs/member_types').Member;
 var conn = thrift.createConnection('localhost', 9701);
 var client = thrift.createClient(MemberStore, conn);
 
@@ -13,26 +14,32 @@ module.exports = {
 
 	index: function(req, res){
 		client.all(function (err, members) {
-			res.send(createCollectionResponse(members));
+			res.send(err ? 500 : createCollectionResponse(members));
 		});
 	},
 
 	create: function(req, res){
-
+		client.create(new Member(req.body), function (err) {
+			res.send(err ? 500 : 201);
+		});
 	},
 
 	show: function(req, res){
 		client.get(req.params.member, function (err, client) {
-			res.send(createModelResponse(client));
+			res.send(err ? 500 : createModelResponse(client));
 		});
 	},
 
 	update: function(req, res){
-
+		client.update(new Member(req.body), function (err) {
+			res.send(err ? 500 : 200);
+		});
 	},
 
 	destroy: function(req, res){
-
+		client.remove(req.params.member, function (err, client) {
+			res.send(err ? 500 : 200);
+		});
 	}
 
 };
